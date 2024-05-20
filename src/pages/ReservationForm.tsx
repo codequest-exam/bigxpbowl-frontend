babf0d8f1b97ce0b20b0789a43dfd2fbde2b241d
LauritsSchou <laurits.schou@gmail.com>
2024-05-20 11:56:31 +0200

    added css to reservation form

--- src/pages/ReservationForm.tsx
+++ src/pages/ReservationForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./reservationform.css";
 
 interface ReservationFormData {
   name: string;
@@ -61,104 +62,103 @@ const Reservations: React.FC = () => {
   };
 
   return (
-    <form className="reservation-form" onSubmit={handleFormSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Phone Number:
-        <input
-          type="text"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Number of Participants:
-        <input
-          type="number"
-          name="numParticipants"
-          value={formData.numParticipants}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Activity:
-        <select
-          name="activity"
-          value={formData.activity}
-          onChange={handleInputChange}
-        >
-          <option value="">Select an activity</option>
-          <option value="bowling">Bowling</option>
-          <option value="airhockey">Air Hockey</option>
-          <option value="dining">Dining</option>
-        </select>
-        <button type="button" onClick={handleAddActivity}>
-          Add
-        </button>
-      </label>
-      <br />
-      <label>
-        Date:
-        <input
-          type="date"
-          name="date"
-          value={formData.date}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Start Time:
-        <input
-          type="time"
-          name="startTime"
-          value={formData.startTime}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Duration:
-        <select
-          name="duration"
-          value={formData.duration}
-          onChange={handleInputChange}
-        >
-          <option value="">Select duration</option>
-          <option value="1hr">1 hour</option>
-          <option value="2hr">2 hours</option>
-        </select>
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-      <br />
-      <h3>Chosen Activities:</h3>
-      <ul>
-        {formData.chosenActivities.map((chosenActivity) => (
-          <li key={chosenActivity}>
-            {chosenActivity}
-            <button
-              type="button"
-              onClick={() => handleRemoveActivity(chosenActivity)}
+    <div className="reservation-form-page">
+      <h2 className="reservation-header">Add New Reservation</h2>
+      <div className="reservation-form-container">
+        <form className="reservation-form" onSubmit={handleFormSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Phone Number:
+            <input
+              type="text"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Number of Participants:
+            <input
+              type="number"
+              name="numParticipants"
+              value={formData.numParticipants}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Activity:
+            <select
+              name="activity"
+              value={formData.activity}
+              onChange={handleInputChange}
             >
-              Remove
+              <option value="">Select an activity</option>
+              <option value="bowling">Bowling</option>
+              <option value="airhockey">Air Hockey</option>
+              <option value="dining">Dining</option>
+            </select>
+            <button type="button" onClick={handleAddActivity}>
+              Add
             </button>
-          </li>
-        ))}
-      </ul>
-    </form>
+          </label>
+          <label>
+            Date:
+            <input
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Start Time:
+            <input
+              type="time"
+              name="startTime"
+              value={formData.startTime}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Duration:
+            <select
+              name="duration"
+              value={formData.duration}
+              onChange={handleInputChange}
+            >
+              <option value="">Select duration</option>
+              <option value="1hr">1 hour</option>
+              <option value="2hr">2 hours</option>
+            </select>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <div className="chosen-activities">
+          <h3>Chosen Activities:</h3>
+          <ul>
+            {formData.chosenActivities.map((chosenActivity) => (
+              <li key={chosenActivity}>
+                {chosenActivity}
+                <button
+                  type="button"
+                  onClick={() => handleRemoveActivity(chosenActivity)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
