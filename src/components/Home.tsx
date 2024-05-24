a85152ad9c261638f717663ffdf53346408ee1a4
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-05-24 10:59:11 +0200

    styling for home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,32 +4,27 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <h1 className="home-title">Welcome to Big XP Bowl</h1>
-        <h2 className="home-subtitle">
-          <i>Where the fun never stops rolling!</i>
-        </h2>
-      <video 
+    <div>
+      <video
         src="src/assets/start-video.mp4" // replace with your video path
-        autoPlay 
-        muted 
-        loop 
-        style={{
-          // "position": "fixed",
-          // "right": 0,
-          // "bottom": 0,
-          maxWidth: "80%",
-          maxHeight: "80%",
-          // minWidth: "100%",
-          // minHeight: "100%"
-        }}
-      >
-      </video>
-      {/* <img src={bowlingAlleyImage} alt="Bowling Alley" className="home-image" /> */}
-      <p className="home-paragraph">
-        Read more about our activities <Link to="/about">here</Link> or call us
-        at +45 12 34 56 78 to book an unforgettable experience.
-      </p>
+        autoPlay
+        muted
+        loop
+        id="myVideo"
+      ></video>
+
+      <div className="home-container">
+        <div className="home-text">
+          <h1 className="home-title">Welcome to Big XP Bowl</h1>
+          <h2 className="home-subtitle">
+            <i>Where the fun never stops rolling!</i>
+          </h2>
+          <p className="home-paragraph">
+            Read more about our activities <Link to="/about">here</Link> or call us at +45 12 34 56 78 to book an unforgettable experience.
+          </p>
+          {/* <img src={bowlingAlleyImage} alt="Bowling Alley" className="home-image" /> */}
+        </div>
+      </div>
     </div>
   );
 }
